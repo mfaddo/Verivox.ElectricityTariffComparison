d8197e391ba86267ec1309cb3a1d3215e43ad6d7
INTERACTTS\Mahmoud.Faddo <mahmoud.faddo@interactts.com>
2024-05-29 18:18:07 +0300

    add installation script

---
+++ Verivox.ElectricityTariffComparison/install_and_run.sh
@@ -0,0 +1,23 @@
+﻿#!/bin/bash
+
+# Update package lists
+sudo apt-get update
+
+# Install .NET SDK
+wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+
+sudo apt-get update
+sudo apt-get install -y apt-transport-https
+sudo apt-get update
+sudo apt-get install -y dotnet-sdk-6.0
+
+# Navigate to the project directory
+cd Verivox.ElectricityTariffComparison
+
+# Restore NuGet packages
+dotnet restore
+
+# Run the application
+dotnet run
